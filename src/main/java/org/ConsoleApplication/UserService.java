5999064465f06b861fc2114554ac5bf870ef6d5b
Reder222 <133664246+Reder222@users.noreply.github.com>
2025-05-31 23:00:13 +0300

    Finish Mockito tests for service layer

--- src/main/java/org/ConsoleApplication/UserService.java
+++ src/main/java/org/ConsoleApplication/UserService.java
@@ -6,17 +6,27 @@ import org.dataClasses.UserData;
 import java.util.List;
 
 public class UserService {
-    private UserDAO userDAO;
+    private final UserDAO userDAO;
 
     public UserService() {
         userDAO = new UserDAO();
     }
 
-    public boolean create (String name, String email, int age){
-        if (name == null || email == null || age <= 0){
+    public UserService(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    public boolean create(String name, String email, int age) {
+        if (name == null || name.isEmpty()) {
             return false;
         }
-        if (userDAO.containsEmail(email)){
+        if (email == null || email.isEmpty()) {
+            return false;
+        }
+        if (age <= 0) {
+            return false;
+        }
+        if (userDAO.containsEmail(email)) {
             return false;
         }
         UserData temp = new UserData(name, email, age);
@@ -24,21 +34,31 @@ public class UserService {
         return true;
     }
 
-    public List<UserData> getAll(){
+    public List<UserData> getAll() {
         return userDAO.findAll();
     }
 
-    public UserData getByID(int id){
+    public UserData getByID(int id) {
         return userDAO.findById(id);
     }
 
-    public UserData update (UserData user){
+    public UserData update(UserData user) {
+        if (user.getName() == null || user.getName().isEmpty()) {
+            return null;
+        }
+        if(user.getEmail() == null || user.getEmail().isEmpty()) {
+            return null;
+        }
+        if (user.getAge() <= 0) {
+            return null;
+        }
+
         return userDAO.update(user);
     }
 
-    public boolean delete (int id){
+    public boolean delete(int id) {
         UserData temp = userDAO.findById(id);
-        if(temp != null){
+        if (temp != null) {
             userDAO.delete(id);
             return true;
         }
